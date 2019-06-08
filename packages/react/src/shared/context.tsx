c97ee1810c88fb8500d20b567fbb4cffe7a9eb60
monkindey <monkindey@163.com>
2019-06-08 22:21:16 +0800

    fix(@uform/react): broadcast in TypeScript

--- packages/react/src/shared/context.tsx
+++ packages/react/src/shared/context.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import { IBroadcast } from '@uform/utils'
 
 export const MarkupContext = React.createContext({})
 export const StateContext = React.createContext({})
-export const BroadcastContext = React.createContext({})
+export const BroadcastContext = React.createContext<Partial<IBroadcast>>({})
