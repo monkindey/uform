b2aae3b1e516ac434fd569a65fade1b242d1e093
monkindey <monkindey@163.com>
2019-06-10 22:05:23 +0800

    refactor(@uform/react): markup in TypeScript

--- packages/react/src/shared/context.tsx
+++ packages/react/src/shared/context.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { ISchema } from '@uform/types'
 import { IBroadcast } from '@uform/utils'
 
-export const MarkupContext = React.createContext({})
+export const MarkupContext = React.createContext<Partial<ISchema>>({})
 export const StateContext = React.createContext({})
 export const BroadcastContext = React.createContext<Partial<IBroadcast>>({})
